aeae819d514c1d905c1d1b1612d9399db015cc4a
Adham167 <adhamabdelsalam3040@gmail.com>
2025-07-11 16:11:21 +0300

    add title and subtitle to details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app_1/core/utils/styles.dart';
 import 'package:bookly_app_1/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app_1/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,22 @@ class BookDetailsViewBody extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(horizontal: width * 0.177),
           child: CustomBookImage(),
+        ),
+        SizedBox(height: 43),
+        Text(
+          "The Jungle book",
+          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 4),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            "Rudyard Kipling",
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ),
       ],
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:bookly_app_1/constants.dart';
 import 'package:bookly_app_1/core/utils/app_router.dart';
-import 'package:bookly_app_1/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(BooklyApp());
